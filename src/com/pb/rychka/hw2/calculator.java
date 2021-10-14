87f98ee8627df2fa9039a8172cde2db4af8da9fa
alex rychka <aleksej.rychka@privatbank.ua>
2021-10-14 23:28:20 +0300

    проблема с выводом "на ноль не делить"

---
+++ src/com/pb/rychka/hw2/calculator.java
@@ -0,0 +1,39 @@
+package com.pb.rychka.hw2;
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("введите первое целое число operand1 и затем єнтер");
+        int x;
+        x = scan.nextInt();
+        System.out.println("введите второе  целое число operand2 и затем єнтер");
+        int y;
+        y = scan.nextInt();
+        System.out.println("віберите одно из действий + - : * и затем єнтер");
+        String sign;//+ - /*
+        sign = scan.next();
+        switch (sign) {
+            case "+":
+                System.out.println(x + y);
+                break;
+            case "-":
+                System.out.println(+(x - y));
+                break;
+            case "*":
+                System.out.println(+(x * y));
+                break;
+            case "/":
+                System.out.println(+(x / y));
+                if (y == 0)
+            {System.out.println("на ноль не делить");
+
+            break;}
+
+        }
+
+
+    }
+}
+
+
+
